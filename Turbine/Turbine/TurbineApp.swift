857da4123491a0be9a409340b6c483cd7e7dc81b
LiquidInfinity <braziel.jones@gmail.com>
2026-01-31 13:08:38 -0500

    - Xcode Project Init

---
+++ Turbine/Turbine/TurbineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurbineApp.swift
+//  Turbine
+//
+//  Created by Malik on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct TurbineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Turbine/TurbineTests/TurbineTests.swift
@@ -0,0 +1,17 @@
+//
+//  TurbineTests.swift
+//  TurbineTests
+//
+//  Created by Malik on 1/31/26.
+//
+
+import Testing
+@testable import Turbine
+
+struct TurbineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
